b4053080eb0cc9b06153fbadc09be950efc4f15f
Dmitry Strokov <dmitry.strokov@aurora.dev>
2022-12-08 22:36:58 +0400

    Add impl

---
+++ example/example.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/aurora-is-near/stream-backup/chunks"
+	"github.com/aurora-is-near/stream-backup/messagebackup"
+	"github.com/davecgh/go-spew/spew"
+)
+
+func main() {
+	backup := chunks.Chunks{
+		Dir:             "mybackup",
+		ChunkNamePrefix: "mystream_",
+	}
+	if err := backup.Open(); err != nil {
+		log.Fatal(err)
+	}
+	defer backup.CloseReader()
+
+	if err := backup.SeekReader(50); err != nil {
+		log.Fatal(err)
+	}
+
+	for i := 0; i < 10; i++ {
+		seq, data, err := backup.ReadNext()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var msg messagebackup.MessageBackup
+		if err = msg.Unmarshal(data); err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%d: %v\n", seq, spew.Sdump(msg))
+	}
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/aurora-is-near/stream-backup
+
+go 1.18
+
+require (
+	github.com/davecgh/go-spew v1.1.1
+	github.com/gogo/protobuf v1.3.2
+)
